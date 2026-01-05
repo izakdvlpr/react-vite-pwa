249010350fcd8d47cceb542e2e893a5c411af07a
izakdvlpr <izakdvlpr@gmail.com>
2026-01-05 11:49:35 -0300

    first commit

---
+++ android/app/src/main/java/com/izakdvlpr/reactvitepwa/MainActivity.java
@@ -0,0 +1,5 @@
+package com.izakdvlpr.reactvitepwa;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.izakdvlpr.reactvitepwa',
+  appName: 'react-vite-pwa',
+  webDir: 'dist'
+};
+
+export default config;
